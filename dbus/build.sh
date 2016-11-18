69cfb02ef5709fc49172c8f26c8992c28e848adf
jinlai <hejinlai@smartconn.cc>
2016-11-19 00:18:45 +0800

    Init version, add dbus
    This commit refs #2749.
    
    HOW BUILT
    See build.sh
    
    HOW VERIFIED
    See Tools.list
    
    Change-Id: I94a0aa43bfe966f662d4bda57247083086d9efaa
    Signed-off-by: jinlai <hejinlai@smartconn.cc>

---
+++ dbus/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Cd dbus-1.8.0
+cd ./dbus-1.8.0
+
+#Import cross compile tool
+CROSS_COMPILE_ENVIRONMENT_HOME=/opt/fsl-imx-x11/3.14.52-1.1.0
+source ${CROSS_COMPILE_ENVIRONMENT_HOME}/environment-setup-cortexa7hf-vfp-neon-poky-linux-gnueabi
+unset PKG_CONFIG_SYSROOT_DIR
+
+#Build for imx6ul board
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --libexecdir=/lib --host=arm-poky-linux-gnueabi
+make
+make install DESTDIR=${PWD}/imx6ul
+
+#Build for crosscompile tool
+make clean
+make distclean
+./configure --prefix=${CROSS_COMPILE_ENVIRONMENT_HOME}/sysroots/cortexa7hf-vfp-neon-poky-linux-gnueabi/usr --sysconfdir=${CROSS_COMPILE_ENVIRONMENT_HOME}/sysroots/cortexa7hf-vfp-neon-poky-linux-gnueabi/etc --libexecdir=${CROSS_COMPILE_ENVIRONMENT_HOME}/sysroots/cortexa7hf-vfp-neon-poky-linux-gnueabi/lib --localstatedir=${CROSS_COMPILE_ENVIRONMENT_HOME}/sysroots/cortexa7hf-vfp-neon-poky-linux-gnueabi/var --host=arm-poky-linux-gnueabi
+make
+make install
+
+#Release
+cd ..
+mkdir -p release
+mkdir -p release/usr
+mkdir -p release/usr/lib
+mkdir -p release/usr/share
+
+cp -R ./dbus-1.8.0/imx6ul/etc ./release
+cp -R ./dbus-1.8.0/imx6ul/lib ./release
+cp -R ./dbus-1.8.0/imx6ul/var ./release
+cp -R ./dbus-1.8.0/imx6ul/usr/bin ./release/usr
+cp -R ./dbus-1.8.0/imx6ul/usr/lib/*.so* ./release/usr/lib
+cp -R ./dbus-1.8.0/imx6ul/usr/share/dbus-1 ./release/usr/share
+
