647ebe82efaf99d9072847744bed07d73eca2fda
sibolwolf <hejinlai@smartconn.cc>
2016-12-05 23:33:09 +0800

    update dbus build.sh file
    Change-Id: Ib6d05155bcfddfb6747f9fbe8058323fa25837b1

--- dbus/build.sh
+++ dbus/build.sh
@@ -7,6 +7,8 @@ cd ./dbus-1.8.0
 CROSS_COMPILE_ENVIRONMENT_HOME=/opt/fsl-imx-x11/3.14.52-1.1.0
 source ${CROSS_COMPILE_ENVIRONMENT_HOME}/environment-setup-cortexa7hf-vfp-neon-poky-linux-gnueabi
 
+autoreconf --force --install
+
 #Build for imx6ul board
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var  --host=arm-poky-linux-gnueabi
 make
