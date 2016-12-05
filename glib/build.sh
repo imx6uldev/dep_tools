d3e489cd16832f649f5a039bb09a182639920a4a
sibolwolf <hejinlai@smartconn.cc>
2016-12-05 21:33:14 +0800

    add glib which depend on libffi, zlib and perl
    Change-Id: I9a1b783eef791799ac823befbb93e976859c20b6
    Signed-off-by: sibolwolf <hejinlai@smartconn.cc>

---
+++ glib/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Build
+cd glib-2.46.2
+
+CROSS_COMPILE_ENVIRONMENT_HOME=/opt/fsl-imx-x11/3.14.52-1.1.0
+source ${CROSS_COMPILE_ENVIRONMENT_HOME}/environment-setup-cortexa7hf-vfp-neon-poky-linux-gnueabi
+
+#Build for imx6ul board
+unset PKG_CONFIG_SYSROOT_DIR
+./configure --prefix=/usr --host=arm-poky-linux-gnueabi
+make
+make install DESTDIR=${PWD}/imx6ul
+
+#Build for crosscompile tool
+make clean
+make distclean
+./configure --prefix=${CROSS_COMPILE_ENVIRONMENT_HOME}/sysroots/cortexa7hf-vfp-neon-poky-linux-gnueabi/usr --host=arm-poky-linux-gnueabi
+make
+make install
+
+cd -
+
+#Release
+mkdir -p release
+mkdir -p release/usr/bin
+mkdir -p release/usr/lib
+mkdir -p release/usr/share
+
+cp -R ./glib-2.46.2/imx6ul/usr/bin/* ./release/usr/bin
+cp -R ./glib-2.46.2/imx6ul/usr/lib/*.so* ./release/usr/lib
+cp -R ./glib-2.46.2/imx6ul/usr/share/* ./release/usr/share
